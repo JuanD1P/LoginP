d2f0e693e09b0f48464487f2d767df969c5612ef
JuanD1P <juanndieg2005@gmail.com>
2025-03-23 18:22:14 -0500

    BD

--- Backend/utils/db.js
+++ Backend/utils/db.js
@@ -1,10 +1,11 @@
 import mysql from 'mysql';
 
 const con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "proyectoi"
+    host: "birmhtjrkgcxakmg6uqc-mysql.services.clever-cloud.com", // Host de Clever Cloud
+    user: "ulfompkq2wfmaagj", // Usuario de Clever Cloud
+    password: "QfIRfXyh9vIsZOHy0aoh", // Contraseña de Clever Cloud
+    database: "birmhtjrkgcxakmg6uqc", // Base de datos de Clever Cloud
+    port: 3306, // Puerto de MySQL
 });
 
 con.connect((err) => {
